8a9d4bd1fb3dd483dd94b1cf3a88ca51b0ebe21e
Yoshiaki Satoh <y-sato@mediba.jp>
2017-12-22 17:31:46 +0900

    1st commit ログイン画面らしきものをFragmentで作ってMainActivityに貼り付けた

--- app/src/main/java/jp/mediba/ysato/androidarchitecturecomponentslearning/MainActivity.kt
+++ app/src/main/java/jp/mediba/ysato/androidarchitecturecomponentslearning/MainActivity.kt
@@ -1,12 +1,17 @@
 package jp.mediba.ysato.androidarchitecturecomponentslearning
 
-import android.support.v7.app.AppCompatActivity
+import android.net.Uri
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
 
-class MainActivity : AppCompatActivity() {
+class MainActivity :  AppCompatActivity(), MastodonFragment.OnFragmentInteractionListener{
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    override fun onFragmentInteraction(uri: Uri) {
+
     }
 }
